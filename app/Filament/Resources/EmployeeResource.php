89e605bca65c7771dbb3dae6ae05b984f723ed71
dennisyd <dennisyd@alum.mit.edu>
2023-12-16 15:28:01 -0600

    Customize Filament Employee Notification

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -14,6 +14,7 @@ use Filament\Forms\Set;
 use Filament\Infolists\Components\Section;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Infolist;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Filters\Indicator;
@@ -217,6 +218,13 @@ class EmployeeResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
+                    ->successNotification(
+                        Notification::make()
+                            ->success()
+                            ->title('Employees deleted.')
+                            ->body('The employee deleted successfully')
+                    )
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
--- app/Filament/Resources/EmployeeResource/Pages/CreateEmployee.php
+++ app/Filament/Resources/EmployeeResource/Pages/CreateEmployee.php
@@ -4,9 +4,23 @@ namespace App\Filament\Resources\EmployeeResource\Pages;
 
 use App\Filament\Resources\EmployeeResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateEmployee extends CreateRecord
 {
     protected static string $resource = EmployeeResource::class;
+
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Employee created';
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Employees created.')
+            ->body('The employee created successfully');
+    }
 }
